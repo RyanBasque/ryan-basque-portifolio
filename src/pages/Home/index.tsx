2ce8692e7c9f95eecce2bed8cd0c6b5f9fa93919
Ryan Basque <ryan.basque.maitha@trademaster.com.br>
2021-09-24 14:04:22 -0300

    feat: firebase hosting

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { HomeFirstView, HomeSecondView } from '../../components/molecules';
 import { PageDefault } from '../../components/organisms';
 
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
 
 const Home = () => {
-    const firebaseConfig = {
-        apiKey: "AIzaSyCY1gvzEavy8dMZezjqk8EDkXWOezRa-1c",
-        authDomain: "ryan-basque-portifolio-fa8f4.firebaseapp.com",
-        databaseURL: "https://ryan-basque-portifolio-fa8f4-default-rtdb.firebaseio.com",
-        projectId: "ryan-basque-portifolio-fa8f4",
-    };
-
-    const app = firebase.initializeApp(firebaseConfig);
-
-    useEffect(() => {
-        async function anyNameFunction() {
-            try {
-            } catch (e) {
-                console.log(e);
-            }
-        }
-        anyNameFunction();
-      }, []);
-
     return (
         <PageDefault>
             <HomeFirstView />
