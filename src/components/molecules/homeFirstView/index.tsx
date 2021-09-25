1e6e603471e6152ca829101800847530007ba24b
Ryan Basque <ryan.basque.maitha@trademaster.com.br>
2021-09-25 14:32:13 -0300

    feat: more animayions

--- src/components/molecules/homeFirstView/index.tsx
+++ src/components/molecules/homeFirstView/index.tsx
@@ -1,50 +1,66 @@
-import React, { Fragment }  from 'react';
+import React, { Fragment } from "react";
 
-import { 
-    ButtonContainer,
-    Container, 
-    ImageContainer, 
-    ResponsiveContainer, 
-    TextContainer 
-} from './styles';
+import {
+  ButtonContainer,
+  Container,
+  ImageContainer,
+  ResponsiveContainer,
+  TextContainer,
+} from "./styles";
 
-import profilePic from '../../../assets/images/profile-pic.png';
-import arrowIcon from '../../../assets/icons/arrow-down.svg';
+import profilePic from "../../../assets/images/profile-pic.png";
+import arrowIcon from "../../../assets/icons/arrow-down.svg";
 
 const HomeFirstView = () => {
+  const scrollToSecondView = (): void => {
+    const elementHeight =
+      document.getElementById("firstContainer")?.offsetHeight;
+    elementHeight && window.scrollTo(0, elementHeight + 60);
+  };
 
-    const scrollToSecondView = (): void => {
-        const elementHeight = document.getElementById('firstContainer')?.offsetHeight;
-        elementHeight && window.scrollTo(0, elementHeight + 60);
-    };
-
-    return (
-        <Fragment>
-            <Container id="firstContainer">
-                <ResponsiveContainer>
-                    <TextContainer>
-                        <h1>Oi, meu nome é <span>&lt;Ryan Basque/&gt;</span> <br /> Eu <span>penso e construo</span> interfaces de sistemas.</h1>
-                        <p>Programador Front-end, entusiasta à área de <span>UX/UI</span> , curioso e determinado.</p>
-                        <p>
-                            Estudo programação web desde os 16 anos, atualmente, tenho 18, com conhecimentos avançados em <span>React, 
-                            React Native, Angular, HTML, CSS, JavaScript, Typescript, Figma, Git, GitHub, Bitbucket, NPM e Yarn.</span> 
-                        </p>
-                        <p>Também estudo <span>Python, Java, SQL, IA - NodeRed / Jupyter, Agile Software Design e UX/UI.</span> </p>
-                    </TextContainer>
-                    <ImageContainer>
-                        <figure>
-                            <img src={profilePic} alt="Ryan Basque profile" />
-                        </figure>
-                    </ImageContainer>
-                </ResponsiveContainer>
-                <ButtonContainer>
-                    <button onClick={scrollToSecondView}>
-                        <img src={arrowIcon} alt="Arrow Down" />
-                    </button>
-                </ButtonContainer>
-            </Container>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Container id="firstContainer">
+        <ResponsiveContainer>
+          <TextContainer data-aos="fade-up-right">
+            <h1>
+              Oi, meu nome é <span>&lt;Ryan Basque/&gt;</span> <br /> Eu{" "}
+              <span>penso e construo</span> interfaces de sistemas.
+            </h1>
+            <p>
+              Programador Front-end, entusiasta à área de <span>UX/UI</span> ,
+              curioso e determinado.
+            </p>
+            <p>
+              Estudo programação web desde os 16 anos, atualmente, tenho 18, com
+              conhecimentos avançados em{" "}
+              <span>
+                React, React Native, Angular, HTML, CSS, JavaScript, Typescript,
+                Figma, Git, GitHub, Bitbucket, NPM e Yarn.
+              </span>
+            </p>
+            <p>
+              Também estudo{" "}
+              <span>
+                Python, Java, SQL, IA - NodeRed / Jupyter, Agile Software Design
+                e UX/UI.
+              </span>{" "}
+            </p>
+          </TextContainer>
+          <ImageContainer data-aos="zoom-in-right">
+            <figure>
+              <img src={profilePic} alt="Ryan Basque profile" />
+            </figure>
+          </ImageContainer>
+        </ResponsiveContainer>
+        <ButtonContainer>
+          <button onClick={scrollToSecondView}>
+            <img src={arrowIcon} alt="Arrow Down" />
+          </button>
+        </ButtonContainer>
+      </Container>
+    </Fragment>
+  );
 };
 
 export default HomeFirstView;
