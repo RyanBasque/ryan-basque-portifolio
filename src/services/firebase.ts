49341dca7518aa8e5eedbb33e6b6b690efdaae4c
Ryan Basque <ryan.basque.maitha@trademaster.com.br>
2021-09-26 19:16:12 -0300

    feat: dotenv react app api key

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,13 +5,13 @@ import { getAnalytics } from 'firebase/analytics';
 import { generateId } from '../utils/idGenerator';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCY1gvzEavy8dMZezjqk8EDkXWOezRa-1c',
-    authDomain: `ryan-basque-portifolio-fa8f4.firebaseapp.com`, 
-    databaseURL: `https://ryan-basque-portifolio-fa8f4-default-rtdb.firebaseio.com`,
-    projectId: 'ryan-basque-portifolio-fa8f4',
-    storageBucket: `ryan-basque-portifolio-fa8f4.appspot.com`,
-    messagingSenderId: '313152388697',
-    appId: '1:313152388697:web:6a2c615e915f1ceb63fb48',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`, 
+    databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+    storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -26,8 +26,6 @@ const getData = (path: string, callback: any) => {
       const response = snapshot.val();
       callback(response);
     });
-
-    return 'iaush';
 }
 
 const postData = (path: string, value: any) => {
